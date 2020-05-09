34b1a38ebba22d8b67abc78d6f3fca5b8f63b412
Mike Leone <mleone896@gmail.com>
2020-05-09 12:11:18 -0400

    add test for counts

--- parser_test.go
+++ parser_test.go
@@ -16,6 +16,16 @@ func readFile() *os.File {
 	return file
 }
 
+func getTestMap() map[string]int {
+	testData := make(map[string]int)
+	testData["burger.electric.ai"] = 2
+	testData["burger.letters.com"] = 1
+	testData["d104.electric.ai"] = 3
+	testData["unicomp6.electric.ai"] = 4
+
+	return testData
+}
+
 func TestParse(t *testing.T) {
 
 	lines, err := Parse(readFile())
@@ -28,4 +38,30 @@ func TestParse(t *testing.T) {
 		t.Errorf("expected 10 items to be parsed got %d", len(lines))
 	}
 
+}
+
+func TestCalculatedUrlCounts(t *testing.T) {
+
+	contents := readFile()
+	counts := getCounts(contents)
+
+	if !eq(counts, getTestMap()) {
+		t.Errorf("Exptected %v to equal %v", counts, getTestMap())
+	}
+
+}
+
+// helper func
+func eq(a, b map[string]int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for k, v := range a {
+		if w, ok := b[k]; !ok || v != w {
+			return false
+		}
+	}
+
+	return true
 }
